60355707847795cd6368c26e27396916c103826b
fralps <francois.loupias@orange.fr>
2020-02-04 16:29:21 +0100

    precompile

--- Gemfile
+++ Gemfile
@@ -89,7 +89,7 @@ gem 'jquery-rails'
 gem 'devise'
 
 # Chart gems
-gem 'chartkick'
+gem 'chartkick', '~> 2.2.0'
 gem 'groupdate'
 
 # Social media buttons gem
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,7 +11,6 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join('lib')
 Rails.application.config.assets.paths << Rails.root.join('vendor')
-Rails.application.config.assets.precompile += %w( Chart.bundle )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
